69ce49bcea145f7e10962ee20742cbd85ce820f6
CodeWithArnab <arnabdebnath1972@gmail.com>
2023-08-11 18:52:51 +0530

    chart done

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  chart: any;
+  time = [
+    '10:30 AM',
+    '11:30 AM',
+    '12:30 PM',
+    '1:30 PM',
+    '2:30 PM',
+    '3:30 PM',
+    '4:30 PM',
+    '5:30 PM',
+  ];
+  apiCalls = [3000, 4500, 5000, 7546, 7000, 8000, 6400, 9000];
+  constructor() {}
 
   ngOnInit(): void {
+    this.chart = new Chart('api-calls', {
+      type: 'line',
+      data: {
+        labels: this.time,
+        datasets: [
+          {
+            data: this.apiCalls,
+            borderColor: '#7549FF',
+            fill: false,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+            
+            
+          ],
+        },
+        legend: {
+          display: false,
+        },
+      },
+    });
   }
-
 }
