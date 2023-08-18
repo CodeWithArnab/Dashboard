f9073de17c5d71b8a3bc8176da7255e7035f38fc
CodeWithArnab <arnabdebnath1972@gmail.com>
2023-08-18 11:08:09 +0530

    second

---
+++ data.js
@@ -0,0 +1,58 @@
+ module.exports  = {
+  userData: {
+    projects: 3,
+    requests: 3456,
+    users: 3,
+    storage: "128/500",
+  },
+  apiCallData: {
+    time: [
+      "10:30 AM",
+      "11:30 AM",
+      "12:30 PM",
+      "1:30 PM",
+      "2:30 PM",
+      "3:30 PM",
+      "4:30 PM",
+      "5:30 PM",
+    ],
+    apiCalls: [3000, 4500, 5000, 7546, 7000, 8000, 6400, 9000],
+  },
+  plData: {
+    pieLabels: ["Re-used APIs", "Webhooks", "API Calls"],
+    pieData: [36, 38, 25],
+  },
+  currentPlanData:{
+    pricing:99,
+    projects:33.33,
+    users:75,
+    requests:50
+  },
+  activityData:[
+    {
+      username: 'James Anderson',
+      img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg',
+      log: 'Called “Books-API” with the JavaScript webhook and commented. “Books-API was readily available with proper documentation and reliability of a proper API. It was just a webhook away from application.”',
+    },
+    {
+      username: 'Vector Sam',
+      img: 'https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=612x612&w=0&k=20&c=eU56mZTN4ZXYDJ2SR2DFcQahxEnIl3CiqpP3SOQVbbI=',
+      log: 'Called “Books-API” with the JavaScript webhook and commented. “Books-API was readily available with proper documentation and reliability of a proper API. It was just a webhook away from application.”',
+    },
+    {
+      username: 'Nether Stone',
+      img: 'https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg',
+      log: 'Called “Books-API” with the JavaScript webhook and commented. “Books-API was readily available with proper documentation and reliability of a proper API. It was just a webhook away from application.”',
+    },
+    {
+      username: 'Jeff Sussex',
+      img: 'https://t3.ftcdn.net/jpg/04/60/91/88/360_F_460918802_XVCymFr7MoziFpnInbTDvrlblYhvAOi2.jpg',
+      log: 'Called “Books-API” with the JavaScript webhook and commented. “Books-API was readily available with proper documentation and reliability of a proper API. It was just a webhook away from application.”',
+    },
+    {
+      username: 'Oliver Anderson',
+      img: 'https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-3763188.jpg&fm=jpg',
+      log: 'Called “Books-API” with the JavaScript webhook and commented. “Books-API was readily available with proper documentation and reliability of a proper API. It was just a webhook away from application.”',
+    },
+  ]
+};
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const data  = require("./data");
 
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(cors()); //allow cross origin requests from any domain to access this server
 
 app.use(express.urlencoded({ extended: true })); //parse urlencoded data from request body
-app.get("/notes", (req, res) => {
-  res.send("notes");
+app.get("/data", (req, res) => {
+  console.log(data);
+  res.send(data);
 });
 app.listen(
     port,
